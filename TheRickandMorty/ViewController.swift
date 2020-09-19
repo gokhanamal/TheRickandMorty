cb1e4e4f61ddb2e243db78ce8f90d8425cf8fa2e
Gokhan Namal <gokhanamal@gmail.com>
2020-09-19 20:07:59 +0300

    Initial Commit

---
+++ TheRickandMorty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheRickandMorty
+//
+//  Created by Gokhan Namal on 19.09.2020.
+//  Copyright © 2020 Gokhan Namal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
